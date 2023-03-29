f902868d4869c4b6f6e0c2ab62b7a3fb1e7bb4a4
GabrielRibeiro0 <126327542+GabrielRibeiro0@users.noreply.github.com>
2023-03-29 13:55:31 -0300

    Add files via upload

---
+++ Capítulo 2/2.1.py
@@ -0,0 +1,5 @@
+2.1 – Mensagem simples: Armazene uma mensagem em uma variável e, em
+seguida, exiba essa mensagem.
+
+mensagem = "hello world!"
+print(mensagem)
---
+++ Capítulo 2/2.9.py
@@ -0,0 +1,6 @@
+2.9 – Número favorito: Armazene seu número favorito em uma variável. Em
+seguida, usando essa variável, crie uma mensagem que revele o seu número
+favorito. Exiba essa mensagem.
+
+number = 2
+print(str(number))
