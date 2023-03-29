0982246cbb710290f512e580870272de57815b0d
GabrielRibeiro0 <126327542+GabrielRibeiro0@users.noreply.github.com>
2023-03-29 13:58:28 -0300

    Add files via upload

---
+++ Capítulo 2/2.10.py
@@ -0,0 +1,15 @@
+2.10 – Acrescentando comentários: Escolha dois dos programas que você
+escreveu e acrescente pelo menos um comentário em cada um. Se você não
+tiver nada específico para escrever porque o programa é muito simples no
+momento, basta adicionar seu nome e a data de hoje no início de cada arquivo
+de programa. Em seguida, escreva uma frase que descreva o que o programa
+faz.
+
+name = "\tCarlos Gabriel\n"
+print(name)
+
+print(name.lstrip()) #lstrip apaga o espaço da esquerda
+
+print(name.rstrip()) #rstrip apaga o espaço da direita
+
+print(name.strip()) #strip apaga o espaço de ambos os lados
