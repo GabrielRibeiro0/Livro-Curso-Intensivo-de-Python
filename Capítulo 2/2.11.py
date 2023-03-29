62a644a46c7d3b1ab24171a6c7dcdd39abfb21de
GabrielRibeiro0 <126327542+GabrielRibeiro0@users.noreply.github.com>
2023-03-29 13:56:30 -0300

    Delete Capítulo 2 directory

--- Capítulo 2/2.11.py
+++
@@ -1,4 +0,0 @@
-2.11 – Zen de Python: Digite import this em uma sessão de terminal de Python
-e dê uma olhada nos princípios adicionais.
-
-import this
--- Capítulo 2/2.9.py
+++
@@ -1,6 +0,0 @@
-2.9 – Número favorito: Armazene seu número favorito em uma variável. Em
-seguida, usando essa variável, crie uma mensagem que revele o seu número
-favorito. Exiba essa mensagem.
-
-number = 2
-print(str(number))
