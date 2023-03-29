d86ed331ac02e069fb3932e6a4ec98ce9358a66f
GabrielRibeiro0 <126327542+GabrielRibeiro0@users.noreply.github.com>
2023-03-29 13:58:14 -0300

    Add files via upload

---
+++ Capítulo 2/2.3.py
@@ -0,0 +1,6 @@
+2.3 – Mensagem pessoal: Armazene o nome de uma pessoa em uma variável e
+apresente uma mensagem a essa pessoa. Sua mensagem deve ser simples,
+como “Alô Eric, você gostaria de aprender um pouco de Python hoje?”.
+
+name = "Gabriel"
+print("Alô " + name + ", você gostaria de aprender um pouco de python hoje?")
---
+++ Capítulo 2/2.5.py
@@ -0,0 +1,6 @@
+2.5 – Citação famosa: Encontre uma citação de uma pessoa famosa que você
+admire. Exiba a citação e o nome do autor. Sua saída deverá ter a aparência
+a seguir, incluindo as aspas: Albert Einstein certa vez disse: “Uma pessoa que
+nunca cometeu um erro jamais tentou nada novo.”
+
+print('Sócrates certa vez disse: "Sábio é aquele que conhece os limites da própia ignorância')
