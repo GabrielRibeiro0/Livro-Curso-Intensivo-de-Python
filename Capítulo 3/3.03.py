948a9b041fed5abfe43410de876bce3cff8a66ef
GabrielRibeiro0 <126327542+GabrielRibeiro0@users.noreply.github.com>
2023-04-01 14:42:58 -0300

    Add files via upload

---
+++ Capítulo 3/3.03.py
@@ -0,0 +1,13 @@
+3.3 – Sua própria lista: Pense em seu meio de transporte preferido, como
+motocicleta ou carro, e crie uma lista que armazene vários exemplos. Utilize sua
+lista para exibir uma série de frases sobre esses itens, como “Gostaria de ter
+uma moto Honda”.
+
+------------------------------------------------------------------------------------------
+
+carros = ['Volkswagen', 'Chevrolet', 'Fiat', 'Toyota']
+
+print('os carros da ' + carros[-1] + ' são os melhores')
+print('odeio andar de ' + carros[3])
+print('os carros da ' + carros[1] + ' são muito caros')
+print('adoro andar de ' + carros[2])
---
+++ Capítulo 3/3.04.py
@@ -0,0 +1,12 @@
+3.4 – Lista de convidados: Se pudesse convidar alguém, vivo ou morto, para o
+jantar, quem você convidaria? Crie uma lista que inclua pelo menos três pessoas
+que você gostaria de convidar para jantar. Em seguida, utilize sua lista para
+exibir uma mensagem para cada pessoa, convidando-a para jantar.
+
+------------------------------------------------------------------------------------------
+
+convidados = ['Einstein', 'Tesla', 'Darwin']
+
+print('\n' + convidados[0] +', você está cordialmente convidado para um jantar.')
+print('\n' + convidados[1] +', você está cordialmente convidado para um jantar.')
+print('\n' + convidados[2] +', você está cordialmente convidado para um jantar.')
